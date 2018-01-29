707ffed8e162978b2650adefe102083820419599
neogeoss <neogeo-s@hanmail.net>
2018-01-29 10:09:24 +0900

    초기 커밋.

---
+++ ChartExample1/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ChartExample1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.ChartSalary = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartRevenue = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.ChartSalary)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRevenue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChartSalary
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.ChartSalary.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.ChartSalary.Legends.Add(legend1);
+            this.ChartSalary.Location = new System.Drawing.Point(45, 52);
+            this.ChartSalary.Name = "ChartSalary";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Salary";
+            this.ChartSalary.Series.Add(series1);
+            this.ChartSalary.Size = new System.Drawing.Size(300, 300);
+            this.ChartSalary.TabIndex = 0;
+            this.ChartSalary.Text = "chart1";
+            title1.Name = "SalaryChartT";
+            title1.Text = "Salary Chart";
+            this.ChartSalary.Titles.Add(title1);
+            // 
+            // chartRevenue
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chartRevenue.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chartRevenue.Legends.Add(legend2);
+            this.chartRevenue.Location = new System.Drawing.Point(375, 52);
+            this.chartRevenue.Name = "chartRevenue";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Revenue";
+            this.chartRevenue.Series.Add(series2);
+            this.chartRevenue.Size = new System.Drawing.Size(300, 300);
+            this.chartRevenue.TabIndex = 1;
+            this.chartRevenue.Text = "chart2";
+            title2.Name = "Revenue Chart";
+            title2.Text = "Revenue Chart";
+            this.chartRevenue.Titles.Add(title2);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(600, 378);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 2;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(689, 413);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.chartRevenue);
+            this.Controls.Add(this.ChartSalary);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ChartSalary)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartRevenue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart ChartSalary;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartRevenue;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ ChartExample1/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChartExample1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            using (ChartEntities db = new ChartEntities()) {
+                chartRevenue.DataSource = db.Revenues.ToList();
+                chartRevenue.Series["Revenue"].XValueMember = "Year";
+                chartRevenue.Series["Revenue"].XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Int32;
+                chartRevenue.Series["Revenue"].YValueMembers = "Total";
+                chartRevenue.Series["Revenue"].YValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Double;
+
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*ChartSalary.Series["Salary"].Points.AddXY("Peter", 1000);
+            ChartSalary.Series["Salary"].Points.AddXY("John", 5000);
+            ChartSalary.Series["Salary"].Points.AddXY("Tan", 1500);
+            ChartSalary.Series["Salary"].Points.AddXY("Lucy", 1000);*/
+
+            ChartSalary.Series["Salary"].Points.Add(1000);
+            ChartSalary.Series["Salary"].Points[0].Color = Color.Red;
+            ChartSalary.Series["Salary"].Points[0].AxisLabel = "Peter";
+            ChartSalary.Series["Salary"].Points[0].LegendText = "Peter";
+            ChartSalary.Series["Salary"].Points[0].Label = "5000";
+
+            ChartSalary.Series["Salary"].Points.Add(5000);
+            ChartSalary.Series["Salary"].Points[1].Color = Color.Green;
+            ChartSalary.Series["Salary"].Points[1].AxisLabel = "John";
+            ChartSalary.Series["Salary"].Points[1].LegendText = "John";
+            ChartSalary.Series["Salary"].Points[1].Label = "5000";
+
+            ChartSalary.Series["Salary"].Points.Add(1500);
+            ChartSalary.Series["Salary"].Points[2].Color = Color.Red;
+            ChartSalary.Series["Salary"].Points[2].AxisLabel = "Tan";
+            ChartSalary.Series["Salary"].Points[2].LegendText = "Tan";
+            ChartSalary.Series["Salary"].Points[2].Label = "1500";
+
+            ChartSalary.Series["Salary"].Points.Add(7000);
+            ChartSalary.Series["Salary"].Points[3].Color = Color.Blue;
+            ChartSalary.Series["Salary"].Points[3].AxisLabel = "Lucy";
+            ChartSalary.Series["Salary"].Points[3].LegendText = "Lucy";
+            ChartSalary.Series["Salary"].Points[3].Label = "7000";
+        }
+    }
+}
